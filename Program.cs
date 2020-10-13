791ee3bd28d3b0b26af02901fd03aca7932c5751
Aleksey <nester.a.l@yandex.ru>
2020-10-13 22:11:02 +0300

    literals

--- Program.cs
+++ Program.cs
@@ -7,6 +7,43 @@ namespace CSharpCourse
     {
         static void Main(string[] args)
         {
+            int x = 0b11;
+            int y = 0b1001;
+            int k = 0b10001001;
+            int m = 0b1000_1001;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+            x = 0x1F;
+            y = 0xFF0D;
+            k = 0X1FAB30EF;
+            m = 0X1FAB_30EF;
+
+            Console.WriteLine();
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+            Console.WriteLine();
+
+            Console.WriteLine(4.5e2);
+            Console.WriteLine(3.1E-1);
+
+            Console.WriteLine();
+
+            Console.WriteLine('\x78');
+            Console.WriteLine('\x5A');
+
+            Console.WriteLine('\u0420');
+            Console.WriteLine('\u0421');
+        }
+        public static void Variables()
+        {
             int x = -1;
 
             int y;
