9c45b7979b79ed584257381dc5c4a5e1939c2e2c
Aleksey <nester.a.l@yandex.ru>
2020-10-13 22:01:23 +0300

    variables

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -1;
+
+            int y;
+            y = 2;
+
+            //Int32 x1 = -1;
+
+            //uint z = -1;
+
+            float f = 1.1f;
+
+            double d = 203;
+
+            int x2 = 0;
+            int x3 = new int();
+
+            var a = 1;
+            var b = 1.2;
+
+            //Dictionary<int, string> dict = new Dictionary<int, string>();
+            //var dict = new Dictionary<int, string>();
+
+            //var v;
+
+            decimal money = 3.0m;
+
+            char character = 'A';
+            string name = "John";
+
+            bool canDrive = true;
+            bool canDraw = false;
+
+            object obj1 = 1;
+            object obj3 = "obj2";
+
+            Console.WriteLine(a);
+            Console.WriteLine(name);
+        }
+    }
+}
